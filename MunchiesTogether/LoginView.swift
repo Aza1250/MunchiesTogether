b79a77364cf13f7296a2db871261f52cbe554429
Aziz Zaynutdinov <azizulin1@gmail.com>
2020-09-17 00:21:01 -0700

    Add login & register buttons to the loginView and make their UI look nice

--- MunchiesTogether/LoginView.swift
+++ MunchiesTogether/LoginView.swift
@@ -20,11 +20,48 @@ struct LoginView: View {
 }
 
 struct LogoView: View {
+    
+    @State var index = 0
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             Image("logo")
                 .resizable()
                 .frame(width: 200, height: 200)
+            
+            HStack {
+                Button(action: {
+                    
+                    self.index = 0
+                    
+                }) {
+
+                    Text("Register")
+                        .foregroundColor(self.index == 0 ? .black : .white)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 20)
+                        .frame(width: (UIScreen.main.bounds.width - 50)/2)
+                    
+                }.background(self.index == 0 ? Color.white : Color.clear)
+                 .clipShape(Capsule())
+                
+                Button(action: {
+                    
+                    self.index = 1
+                    
+                }) {
+
+                    Text("Login")
+                        .foregroundColor(self.index == 1 ? .black : .white)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 20)
+                        .frame(width: (UIScreen.main.bounds.width - 50)/2)
+                    
+                }.background(self.index == 1 ? Color.white : Color.clear)
+                 .clipShape(Capsule())
+                
+            }.background(Color.black.opacity(0.1))
+            .clipShape(Capsule())
         }
     }
 }

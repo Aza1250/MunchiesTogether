1c66edba1b51ace2a8e625d0a2068bc3423bd655
Aziz Zaynutdinov <azizulin1@gmail.com>
2020-09-17 12:49:47 -0700

    Add simple fields for user input to login/register

--- MunchiesTogether/LoginView.swift
+++ MunchiesTogether/LoginView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 import UIKit
 
-struct LoginView: View {
+struct HomeView: View {
     var body: some View {
         ZStack {
             LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color1"), Color("Color2")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
@@ -24,7 +24,7 @@ struct LogoView: View {
     @State var index = 0
     
     var body: some View {
-        VStack(spacing: 30) {
+        VStack() {
             Image("logo")
                 .resizable()
                 .frame(width: 200, height: 200)
@@ -62,6 +62,48 @@ struct LogoView: View {
                 
             }.background(Color.black.opacity(0.1))
             .clipShape(Capsule())
+            .padding(.top, 50)
+            
+            
+            LoginView()
+        }
+    }
+}
+
+struct LoginView : View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    var body : some View {
+        VStack {
+            HStack(spacing: 15) {
+                Image(systemName: "envelope")
+                    .foregroundColor(.black)
+                TextField("Enter Email Address", text: self.$email)
+            }.padding(.vertical, 20)
+            
+            Divider()
+            
+            HStack(spacing: 15) {
+                Image(systemName: "lock")
+                    .resizable()
+                    .frame(width: 15, height: 18)
+                    .foregroundColor(.black)
+                SecureField("Enter Password", text: self.$password)
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Image(systemName: "eye")
+                    
+                }
+
+                
+                
+            }.padding(.vertical, 20)
+            
         }
     }
 }
